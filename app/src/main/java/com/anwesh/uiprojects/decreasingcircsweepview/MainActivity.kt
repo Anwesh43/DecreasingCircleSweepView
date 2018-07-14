65a954959b727c739eecf6e10d2e7cb9d9a23e45
Anwesh43 <anweshthecool0@gmail.com>
2018-07-14 23:15:47 +0530

    adding onCompletionListener in Activity

--- app/src/main/java/com/anwesh/uiprojects/decreasingcircsweepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/decreasingcircsweepview/MainActivity.kt
@@ -3,14 +3,18 @@ package com.anwesh.uiprojects.decreasingcircsweepview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.dcsview.DCSView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        DCSView.create(this)
+        val view : DCSView = DCSView.create(this)
         fullScreen()
+        view.addOnCompletionListener {
+            Toast.makeText(this, "${it} animation completed", Toast.LENGTH_SHORT).show()
+        }
     }
 }
 
