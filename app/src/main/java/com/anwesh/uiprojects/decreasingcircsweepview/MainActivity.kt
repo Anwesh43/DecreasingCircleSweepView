03d57f6e3fb400d9845a2fb68510161903e93f40
Anwesh43 <anweshthecool0@gmail.com>
2018-07-14 14:14:29 +0530

    adding DCSView in Activity

--- app/src/main/java/com/anwesh/uiprojects/decreasingcircsweepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/decreasingcircsweepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.decreasingcircsweepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.dcsview.DCSView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DCSView.create(this)
     }
 }
